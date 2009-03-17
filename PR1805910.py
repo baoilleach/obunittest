5deedc03511313ceb65c3d64e6ac3095bd57b344
oboyle <baoilleach@gmail.com>
2009-03-17 17:31:38 +0000

    Adding PR1805910

---
+++ PR1805910.py
@@ -0,0 +1,20 @@
+import pybel
+import sweet
+
+class TestCase(sweet.TestCase):
+    """Regression Test for PR1805910
+
+    Various errors relating to inversion of stereochemistry across Cansmiles
+    and InChI.
+    """
+    def setUp(self):
+        self.smiles = [x.rstrip() for x in open("ProblemChiralitySMILES-2007-10-01.smi", "r").readlines()]
+        self.mols = [pybel.readstring("smi", x) for x in self.smiles]
+
+##    def testIdentity(self):
+##        self.assertSameMol(self.mol, self.serialised)
+
+    def testSMIRetainsStereo(self):
+        for smiles, mol in zip(self.smiles, self.mols):
+            self.assertEqual(smiles, mol.write("smi").rstrip())
+            
