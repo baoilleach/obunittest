ed5557b9d3fc2606c19098817deb606f804b9f9c
oboyle <baoilleach@gmail.com>
2009-03-19 12:09:51 +0000

    Added a test for stereochemistry in a fused ring. Passes!

--- PR1805910.py
+++ PR1805910.py
@@ -20,12 +20,20 @@ class TestCase(sweet.TestCase):
         for smiles, mol in zip(self.smiles, self.mols):
             self.assertEqual(smiles, mol.write("smi").rstrip())
 
-    def testSameCan(self):
+    def testSameCanSpiro(self):
         """Test several representations of the same spiro molecule."""
         can = pybel.readstring("smi", "C1CN[C@]12CCCN2").write("can").split()[0]
         for smile in ['C1CN[C@]12CCCN2', 'C1CN[C@@]21CCCN2',
                        'C1CN[C@@]2(C1)CCN2']:
             mycan = pybel.readstring("smi", smile).write("can").split()[0]
             self.assertEqual(can, mycan, smile)
+
+    def testSameCanFused(self):
+        """Test two representations of the same fused mlecule."""
+        can = pybel.readstring("smi", "C1CCCC2[C@@H]1CCNC2").write("can").split()[0]
+        for smile in ['C1CCCC2[C@@H]1CCNC2', 'C1CC[C@H]2CCNCC2C1']:
+            mycan = pybel.readstring("smi", smile).write("can").split()[0]
+            self.assertEqual(can, mycan, smile)
+
             
             
