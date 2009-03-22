d64e4dd8c3f78a78dd54e36ce8dae726a6a74621
oboyle <baoilleach@gmail.com>
2009-03-22 15:04:24 +0000

    Another cis/trans failure

--- SMILESwriter.py
+++ SMILESwriter.py
@@ -7,15 +7,28 @@ class TestCase(sweet.TestCase):
 
     """
     def setUp(self):
-        self.mol = pybel.readfile("mol", "SMILESwriter_15061.mol").next()
+        filenames = ["SMILESwriter_15061.mol", "SMILESwriter_12803.mol"]
+        self.mols = [pybel.readfile("mol", x).next() for x in filenames]
     def testDoubleBondStereo(self):
-        can = self.mol.write("can")
-        smi = self.mol.write("smi")
+        can = self.mols[0].write("can")
+        smi = self.mols[0].write("smi")
         can_fromsmi = pybel.readstring("smi", smi).write("can")
         for smiles in [can, smi, can_fromsmi]:
             # Assert trans (it's a pretty lame way of doing it,
             # but it works here)
             self.assertEqual(len(smiles.split("/")), 3)
             self.assertEqual(len(smiles.split("\\")), 1)
+    def testAnotherDoubleBondStereo(self):
+        can = self.mols[1].write("can")
+        smi = self.mols[1].write("smi")
+        can_fromsmi = pybel.readstring("smi", smi).write("can")
+        for smiles in [can, smi, can_fromsmi]:
+            # Assert both bonds trans
+            alldown = len(smiles.split("/")) == 5
+            allup = len(smiles.split("\\")) == 5
+            self.assertTrue(allup or alldown,
+                            "%s is not all trans" % smiles.split()[0])
+   
+    
                 
 
